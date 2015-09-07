4ae3e2cc2dc49c2a688e3069bd40241776aad934
Zach <remitius@gmail.com>
2015-09-07 14:56:42 -0500

    Add pagination to workouts controller index action.

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -1,6 +1,11 @@
 class WorkoutsController < ApplicationController
   def index
-    @workouts = Workout.all
+    if params['page'] && Workout.first.exists?
+      p = params['page'].to_i * 10
+      @workouts = Workout.order(:date)[(p-10)...p]
+    else
+      @workouts = Workout.order(:date).first(10)
+    end
   end
 
   def new
