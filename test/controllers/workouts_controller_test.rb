3777e8bedd01fe7ab04550c2548795c87895f61f
Zach <remitius@gmail.com>
2015-09-07 18:41:34 -0500

    Add tests for pagination of the index action of workouts controller

--- test/controllers/workouts_controller_test.rb
+++ test/controllers/workouts_controller_test.rb
@@ -6,6 +6,24 @@ class WorkoutsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "index should show first 10 workouts by default" do
+    get :index
+    assert_select 'span#workout_name', count: 10
+  end
+
+  test "index should show workouts based on page parameter" do
+    #based on 22 fixtures
+    get :index, page: 2
+    assert_select 'span#workout_name', count: 10
+    get :index, page: 3
+    assert_select 'span#workout_name', count: 2
+  end
+
+  test "index response should be valid if workout list is empty" do
+    get :index, page: 5000
+    assert_response :success 
+  end
+
   test "should get new" do
     get :new
     assert_response :success
