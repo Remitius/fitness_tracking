e28edd5b759e054d291662d261584b7a61105c84
Zach <remitius@gmail.com>
2015-09-18 12:53:57 -0500

    Add site layout integration test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+  test "universal header and footer" do
+    get create_path
+    assert_select 'a[href="/"]', count: 1
+  end
+
+  test "link to root path not present in root path's header" do
+    get root_path
+    assert_select 'a[href="/"]', count: 0
+  end
+
+end
