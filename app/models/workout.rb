af55a2ab87649b573998994f944f2f77e54328a8
Zach <remitius@gmail.com>
2015-09-18 22:03:20 -0500

    Change workout form's date field to select box

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -2,6 +2,6 @@ class Workout < ActiveRecord::Base
   has_many :exercises, dependent: :destroy
   validates :name, presence: true, length: { maximum: 35 }
   validates_date :date, allow_nil: false, 
-                 between: ['2000-1-1', 2.years.from_now]
+                 between: ['2012-1-1', 1.year.from_now]
   validates :note, allow_nil: true, length: { maximum: 300 }
 end
