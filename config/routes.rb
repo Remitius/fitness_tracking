1beee4dd32996a49375d19f9d3f6a09a63da32b5
Zach <remitius@gmail.com>
2015-09-25 12:25:45 -0500

    Add /workouts as a valid route for workouts#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'workouts#index'
   get 'workouts/new', as: 'new'
-  resources :workouts, only: [:show, :create, :update, :destroy] do
+  resources :workouts, only: [:show, :create, :update, :destroy, :index] do
     resources :exercises, only: [:create, :update, :destroy]
   end
 
