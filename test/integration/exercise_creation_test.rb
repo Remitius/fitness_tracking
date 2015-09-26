38a89437c7513bd1015b657901df9c0c7ed96a30
Zach <remitius@gmail.com>
2015-09-26 00:20:30 -0500

    Add assertions regarding exercise creation error messages and improve flash error partial

--- test/integration/exercise_creation_test.rb
+++ test/integration/exercise_creation_test.rb
@@ -11,6 +11,7 @@ class ExerciseCreationTest < ActionDispatch::IntegrationTest
                       exercise: { name: "sprints", workout: @workout }
     assert_template 'workouts/show'
     assert_select "ul[class='exercise']", count: 1
+    assert_select "h3[class='exercise_name']", count: 1
   end
 
   test "invalid creation" do
@@ -19,6 +20,14 @@ class ExerciseCreationTest < ActionDispatch::IntegrationTest
                       exercise: { name: "", workout: @workout }
     assert_template 'workouts/show'
     assert_select "ul[class='exercise']", count: 0
+    assert_select "h3[class='exercise_name']", count: 0
   end
 
+  test "invalid creation error messages" do
+    get workout_path(@workout)
+    post_via_redirect workout_exercises_path(@workout), 
+                      exercise: { name: "", workout: @workout }
+    assert_select "div[class='flash_error']", /Name can't be blank/i
+
+  end
 end
