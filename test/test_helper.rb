c2f9a1ce64401da6a5582490d2ff3cdf3df89c59
Zach <remitius@gmail.com>
2015-10-05 12:42:05 -0500

    Correct the syntax for the use of save parameter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,19 +8,19 @@ class ActiveSupport::TestCase
 
   def valid_workout(save: false)
     w = Workout.new(name: 'lower body', date: '2015-3-3')
-    w.save if :save
+    w.save if save
     w
   end
 
   def valid_exercise(workout, save: false)
     e = Exercise.new(name: 'barbell flies', note: 'hi', workout: workout)
-    e.save if :save
+    e.save if save
     e
   end
 
   def valid_exercise_set(exercise, save: false)
     es = ExerciseSet.new(pounds: 500.5, reps: 1, exercise: exercise)
-    es.save if :save
+    es.save if save
     es
   end
 end
