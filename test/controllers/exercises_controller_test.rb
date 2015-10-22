f2152141808bd73928038b3ce60767c178b55c63
Remitius <remitius@gmail.com>
2015-10-21 23:10:41 -0500

    Remove useless setup code and empty tests

--- test/controllers/exercises_controller_test.rb
+++ test/controllers/exercises_controller_test.rb
@@ -1,21 +1,4 @@
 require 'test_helper'
 
 class ExercisesControllerTest < ActionController::TestCase
-  def setup
-    @workout = Workout.create(id: 1, name: "sprints", date: "2012-3-20")
-    @exercise = Exercise.create(name: "bench press", note: "PR",
-                                workout: @workout)
-  end
-
-  test "valid update should properly alter the record" do
-    
-  end
-
-  test "invalid update should not alter the record" do
-    
-  end
-
-  test "destroy should remove the record" do
-    #assert workout preserved
-  end
 end
