6d5bca88c05b41290faa96799303428111325716
Remitius <remitius@gmail.com>
2015-11-10 00:11:06 -0600

    Add links from workouts#edit and exercises#edit back to their corresponding workout's show page. Move several tests to more appropriate files.

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,26 +13,41 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
 
   test "link to root path not present in root path's header" do
     get root_path
-    assert_select 'a[href="/"]', count: 0
+    assert_select 'a[href="/"]', false
   end
 
-  test "correct workout form elements on workouts#edit" do
+  test "form elements on _workout_form partial" do
     get edit_workout_path(@workout)
+    assert_template partial: '_workout_form'
     assert_select "label[for='workout_name']", count: 1
     assert_select "label[for='workout_date']", count: 1
     assert_select "label[for='workout_note']", count: 1
- end
+  end
 
-  test "correct exercise form elements on workouts#show" do
+  test 'link back to workouts#show page from workouts#edit' do
+    get edit_workout_path(@workout)
+    assert_select "a[href='#{workout_path(@workout.id)}']", count: 1
+  end
+
+  test "exercise form elements onn _exercise_form partial" do
     get workout_path(@workout)
+    assert_template partial: '_exercise_form'    
     assert_select "label[for='exercise_name']", count: 1
     assert_select "label[for='exercise_note']", count: 1
- end
+  end
 
-  test "correct e_set form elements on workouts#show" do
+  test "e_set form elements on workouts#show" do
     get workout_path(@workout)
     assert_select "#e-set-fields"
     assert_select "a", "add set", count: 1
- end
+  end
+
+  test "exercise edit" do
+    exer = valid_exercise(@workout, save: true)
+    get edit_workout_exercise_path(@workout, exer)
+    assert_template partial: '_exercise_form'    
+    assert_select "a[href='#{workout_path(@workout.id)}']", count: 1
+  end
+
 
 end
