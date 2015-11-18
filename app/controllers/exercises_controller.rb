142699519e29c906063d5e0e9ac8b90f4570cfbf
Remitius <remitius@gmail.com>
2015-11-17 20:49:44 -0600

    Display highest weights for each day on line graph on exercises index

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -38,9 +38,7 @@ class ExercisesController < ApplicationController
 
     if Exercise.find_by(name: params[:name])
       exercises = Exercise.all.select{|e| e.name.downcase == params[:name]}
-      @sets = ESet.all.select{|s| s.exercise.name.downcase == params[:name]}
-      @sets.sort! {|a,b| b.exercise.workout.date <=> a.exercise.workout.date}
-      @exercise_data = gather_exercise_data(exercises, @sets)
+      @exercise_data = gather_exercise_data(exercises)
     end
   end
 
@@ -61,16 +59,52 @@ class ExercisesController < ApplicationController
     names.sort!
   end
 
-  def gather_exercise_data(exercises, sets)
-    data = {}
+  def gather_exercise_data(exercises)
+    sets = ESet.all.select{|s| s.exercise.name.downcase == exercises[0].name}
 
+    data = {}
     data[:number_of_instances] = exercises.count
     data[:first_instance], data[:last_instance] = 
         find_first_and_last_instances(exercises)
     data[:lightest_set], data[:heaviest_set] = 
         find_lightest_and_heaviest_sets(sets)
+    data[:formatted_sets] = get_formatted_sets(sets)
 
     data
+  end
+
+  def get_formatted_sets(sets)
+    sets = sets.sort! {|a,b| b.exercise.workout.date <=> a.exercise.workout.date}
+    formatted_sets = []
+    sets.each do |s|
+      h = {}
+      h[:date] = s.exercise.workout.date.to_s
+      h[:workout_id] = s.exercise.workout_id
+      h[:pounds] = s.pounds
+      h[:reps] = s.reps
+      formatted_sets << h
+    end
+    formatted_sets = get_heaviest_daily_sets(formatted_sets) if params[:view] == 'charts'
+    formatted_sets
+  end
+
+  def get_heaviest_daily_sets(_set_hashes)
+    set_hashes = _set_hashes.select { |s| s[:pounds] }
+
+    current = 0
+    while (current < set_hashes.length-1)
+      if set_hashes[current][:date] == set_hashes[current+1][:date]
+        if (set_hashes[current][:pounds] < set_hashes[current+1][:pounds])
+          set_hashes.delete_at(current)
+        else
+          set_hashes.delete_at(current+1)
+        end
+
+      else
+        current += 1
+      end
+    end
+    set_hashes
   end
 
   def find_lightest_and_heaviest_sets(_sets)
