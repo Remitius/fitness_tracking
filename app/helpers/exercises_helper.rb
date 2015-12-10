4716c1f08806715e3dbfeccf031365e40171d464
Remitius <remitius@gmail.com>
2015-12-10 01:40:32 -0600

    Increase specificity of method name

--- app/helpers/exercises_helper.rb
+++ app/helpers/exercises_helper.rb
@@ -1,5 +1,5 @@
 module ExercisesHelper
-  def build_start_date_options
+  def build_graph_start_date_options
     options = {'First occurrence' => nil}
     [1,2,4,6,8,10].each{ |i| options[pluralize(i, 'month') + ' ago'] = i }
     [1,2,3].each{ |i| options[pluralize(i, 'year') + ' ago'] = i*12 }
