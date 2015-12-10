f487cc94f295892f3483775c58080fdecc7fd038
Remitius <remitius@gmail.com>
2015-12-09 19:26:17 -0600

    Various changes reflecting the relationship between users and workouts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
+  has_many :workouts, dependent: :destroy
   has_secure_password
 
   validates :password, length: { minimum: 6, maximum: 30 }
   validates :username, presence: true, length: { minimum: 5, maximum: 25 }, 
     format: { with: /\A\S*\z/ }, uniqueness: { case_sensitive: false }
-
 end
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,10 +1,25 @@
+class WorkoutValidator < ActiveModel::Validator
+  def validate(workout)
+    if workout.user_id.nil?
+      workout.errors[:_] << "Workout's user id doesn't exist (memory)"
+    elsif !User.exists?(workout.user_id)
+      workout.errors[:_] << "Workout's user id doesn't exist (database)"
+    end
+  end
+
+end
+
 class Workout < ActiveRecord::Base
   MAX_EXERCISES = 12
   MAX_NAME_LENGTH = 35
   MAX_NOTE_LENGTH = 300
+  
   has_many :exercises, dependent: :destroy
+  belongs_to :user
+
   validates :name, presence: true, length: { maximum: MAX_NAME_LENGTH }
   validates_date :date, allow_nil: false, 
                  between: ['2012-1-1', 1.year.from_now]
   validates :note, allow_nil: true, length: { maximum: MAX_NOTE_LENGTH }
+  validates_with WorkoutValidator
 end
